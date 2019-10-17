0a6a413507062310d9fccc9a69f27ad729f1a94d
Anastasiya Medvedeva <>
2019-10-17 18:47:02 +0300

    max-heap methods

--- src/max-heap.js
+++ src/max-heap.js
@@ -2,47 +2,55 @@ const Node = require('./node');
 
 class MaxHeap {
 	constructor() {
-		
+		this.root = null;
+		this.parentNodes = [];
 	}
 
 	push(data, priority) {
-		
+		let node = new Node(data, priority);
+		this.insertNode(node);
+		this.shiftNodeUp(node);
 	}
 
 	pop() {
-		
+
 	}
 
 	detachRoot() {
-		
+
 	}
 
 	restoreRootFromLastInsertedNode(detached) {
-		
+
 	}
 
 	size() {
-		
+
 	}
 
 	isEmpty() {
-		
+		if (this.root)
+			return false;
+			else return true;
 	}
 
 	clear() {
-		
+		this.root = null;
+		this.parentNodes = [];
 	}
 
 	insertNode(node) {
-		
+		if (!this.root) {
+			this.root = node;
+		}
 	}
 
 	shiftNodeUp(node) {
-		
+
 	}
 
 	shiftNodeDown(node) {
-		
+
 	}
 }
 
